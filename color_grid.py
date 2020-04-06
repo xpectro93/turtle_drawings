b51df15e28531d5115108a5de92a44b48f71363a
xpextro93 <jonathanandrade@pursuit.org>
2020-04-05 20:50:14 -0400

    add final version of color grid

--- color_grid.py
+++ color_grid.py
@@ -7,20 +7,23 @@ loadWindow.setup(width=1000, height=1000)
 loadWindow.bgcolor("black")
 turtle.speed(0)
 turtle.colormode(255)
-turtle.pensize(1)
+turtle.pensize(3)
 
-def draw_grid(STEP,LENGTH):
-    for i in range(-400, LENGTH, STEP):
-        r = random.randint(0,255)
-        g = random.randint(0,255)
-        b = random.randint(0,255)
-        turtle.pencolor(r,g,b)
+def draw_grid(STEP,LENGTH,COLOR):
+    for i in range(-500, LENGTH, STEP):
+        if COLOR:
 
+            r = random.randint(0,255)
+            g = random.randint(0,255)
+            b = random.randint(0,255)
+            turtle.pencolor(r,g,b)
+        else:
+            turtle.pencolor('black')
 
         #vertical lines 
         turtle.penup()
         #start position
-        turtle.setpos(-400, LENGTH/2)
+        turtle.setpos(-500, LENGTH/2)
         turtle.pendown()
         #ending position
         turtle.setpos(-LENGTH/2 + i, -LENGTH/2)
@@ -31,13 +34,13 @@ def draw_grid(STEP,LENGTH):
         turtle.setpos(LENGTH/2, -400)
         turtle.pendown()
         #ending position
-        turtle.setpos(-400, -LENGTH/2 + i)
+        turtle.setpos(-500, -LENGTH/2 + i)
 
-# draw_grid(25,800)
-x = 100
-y = 1000
+x = 25
+y = 800
+color = True
+
 while x > 0 or y < 0:
-    draw_grid(x,y)
-    x-= 5
-    y-= 5
+    draw_grid(x,y,color)
+    color = not color
     loadWindow.update()
